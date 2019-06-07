31f4bfa89491386b7848df9690211b8233730e04
Annie Truex <artruex@gmail.com>
2019-06-07 04:52:34 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,13 @@
 class Project
-  
-  attr_reader :title, :backer
-  
-  def initialization(title)
+  attr_accessor :title, :backers
+
+  def initialize(title)
     @title = title
     @backers = []
   end
   
   def add_backer(backer)
-    @backers << backer 
+    @backers << backer
     backer.backed_projects << self
   end
-  
 end
